86015024b15234afed5eac7465c1b7e105cfc35a
Ashinee Kesanam <ashineekesanam@gmail.com>
2025-07-26 18:31:03 +0000

    getting the same error show i make any change on the firebase website?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCKS3tXA-FWJ5LgBpYNJbtbw1kh5VVKzjc",
+  authDomain: "sahayak-u279c.firebaseapp.com",
+  projectId: "sahayak-u279c",
+  storageBucket: "sahayak-u279c.firebasestorage.app",
+  messagingSenderId: "1037126053921",
+  appId: "1:1037126053921:web:4abdb69a3e47b546347360",
 };
 
 // Initialize Firebase
